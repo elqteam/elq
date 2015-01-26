01c672b25467cf76c35510deaa36a2bcd9c174b9
Lucas Wiener <lucas@wiener.se>
2015-01-26 11:23:36 +0100

    Created project structure

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+"use strict";
+
+module.exports = function(grunt) {
+    require("load-grunt-tasks")(grunt);
+
+    var config = {
+        jshint: {
+            src: {
+                src: ["src/**/*.js", "*.js"]
+            },
+            test: {
+                src: "test/**/*.js"
+            },
+            options: {
+                jshintrc: true
+            }
+        },
+        jscs: {
+            all: ["src/**/*.js", "test/**/*.js", "*.js"],
+            options: {
+                config: ".jscs.json"
+            }
+        },
+        browserify: {
+            dev: {
+                src: ["src/elq.js"],
+                dest: "build/elq.js",
+                options: {
+                    standalone: "elq",
+                    debug: true
+                }
+            },
+            dist: {
+                src: ["src/elq.js"],
+                dest: "build/elq.js",
+                options: {
+                    standalone: "elq"
+                }
+            }
+        }
+    };
+
+    grunt.initConfig(config);
+
+    grunt.registerTask("build:dev", ["browserify:dev"]);
+    grunt.registerTask("build:dist", ["browserify:dist"]);
+
+    grunt.registerTask("build", ["browserify"]);
+    grunt.registerTask("dist", ["build:dist"]);
+
+    grunt.registerTask("test:style", ["jshint", "jscs"]);
+    grunt.registerTask("test", ["test:style", "build:dev"]); //TODO
+
+    grunt.registerTask("default", ["test"]);
+};
