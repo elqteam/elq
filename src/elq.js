764e18dc72b548374f8967543f8c320119d6460f
Lucas Wiener <lucas@wiener.se>
2015-01-27 12:51:22 +0100

    Can now detect resizes.

--- src/elq.js
+++ src/elq.js
@@ -1,17 +1,26 @@
 "use strict";
 
 var ExtensionHandler = require("./extension/extension-handler");
+var elementResizeDetectorMaker = require("./element-resize-detector");
 
 module.exports = function() {
     var elq = {};
     var extensionHandler = new ExtensionHandler();
+    var elementResizeDetector = elementResizeDetectorMaker(window, $);
+
+    function scan() {
+        elementResizeDetector.listenTo(document.getElementsByClassName("elq"), function(element) {
+            console.log("resized " + $(element).width() + "x" + $(element).height());
+        });
+    }
 
     elq.version = version;
     elq.use = extensionHandler.register.bind(extensionHandler, elq);
     elq.using = extensionHandler.isRegistered.bind(extensionHandler);
     elq.getExtension = extensionHandler.get.bind(extensionHandler);
+    elq.scan = scan;
 
     return elq;
-}
+};
 
 var version = "v0.0.0";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var elqMaker = require("./elq");
 
 module.exports = elqMaker();
