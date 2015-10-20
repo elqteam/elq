7cb8cc1da8f1fc642d9c032eeb2577853bbe82c8
Lucas Wiener <lucas@wiener.se>
2015-10-20 16:33:06 +0200

    Fixed get/hasAttribute bug in elq-breakpoints

--- src/plugin/elq-breakpoints/elq-breakpoints.js
+++ src/plugin/elq-breakpoints/elq-breakpoints.js
@@ -25,7 +25,7 @@ module.exports = {
         });
 
         function activate(element) {
-            if (!elementUtils.getAttribute(element, "elq-breakpoints")) {
+            if (!elementUtils.hasAttribute(element, "elq-breakpoints")) {
                 return;
             }
 
